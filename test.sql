bad528c9c0425b5abb966a452cb742d3c32d175c
mimo0008 <150015612+mimo0008@users.noreply.github.com>
2023-11-20 12:54:50 +0100

    Test Repository

---
+++ test.sql
@@ -0,0 +1,9 @@
+select 
+*
+from Album a 
+order by 1 asc 2 desc
+group by 1,2
+limit 10
+HAVING ab.album = 'Mamma Mia' and datepart(second,a.playtime) <= 180 and genre != 'hip-hop'
+--zusätzliche Info ergänzt.
+--this info is from rock branch
