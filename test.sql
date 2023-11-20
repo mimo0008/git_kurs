3914d71399d8b5d664de2f0113326d7a11f7b3f0
Michael Moser <michael.moser08@gmail.com>
2023-11-20 14:50:03 +0100

    lokale änderung comment2

--- test.sql
+++ test.sql
@@ -8,3 +8,4 @@ HAVING ab.album = 'Mamma Mia' and datepart(second,a.playtime) <= 180 and genre !
 --zusätzliche Info ergänzt.
 --this info is from rock branch
 --lokale Änderung 20.11.
+--lokale änderung nummer 2 
